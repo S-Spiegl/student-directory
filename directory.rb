4b4102f2545fbd97e34709ecc86bd50278a603e1
StevenSpiegl <steven.spiegl@gmail.com>
2022-04-09 03:08:54 +0100

    switched to array of hashes

--- directory.rb
+++ directory.rb
@@ -1,15 +1,26 @@
-students = ["Dr Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone",
-"Alex deLarge", "The Wicked Witch of the West", "Terminator", "Freddy Kruger", "The Joker",
-"Joffrey Baratheon", "Norman Bates", "Nosferatu"]
+students = [
+{name: "Dr Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex deLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Kruger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november},
+{name: "Nosferatu", cohort: :november},
+{name: "Vlad, Impaler", cohort: :november}]
 
 def print_header
   puts "The students of Villains Academy"
   puts "----------------------------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
