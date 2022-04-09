adf48c19b3e2ffae100439f1d2f5492de420062b
StevenSpiegl <steven.spiegl@gmail.com>
2022-04-09 02:50:35 +0100

    refactored some more

--- directory.rb
+++ directory.rb
@@ -1,10 +1,23 @@
-student_count = 11
-
-
 students = ["Dr Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone",
 "Alex deLarge", "The Wicked Witch of the West", "Terminator", "Freddy Kruger", "The Joker",
 "Joffrey Baratheon", "Norman Bates", "Nosferatu"]
-puts "The students of Villains Academy"
- "----------------------------------"
- students.each {|student| puts student}
-print "Overall we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------------------------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+  #this only worked with puts, not with print... gave undefined method otherwise... wonder why...
+end
+
+print_header
+print(students)
+print_footer(students)
